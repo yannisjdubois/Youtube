fcaacd5d9fc083e9f2d4f47d936ce4e753dc4743
y2Dev <dub-y@tuta.io>
2022-07-26 12:25:57 -0400

    Initialisation d'une interface mobile Youtube

---
+++ constantes/categories.js
@@ -0,0 +1,8 @@
+export const categories = [
+    {id:0, titre: "Cinéma" },
+    {id:1, titre: "Jeux-vidéo"},
+    {id:2, titre: "Musiques" },
+    {id:3, titre: "Titre4"},
+    {id:4, titre: "Titre5"},
+
+] ;
---
+++ constantes/videos.js
@@ -0,0 +1,13 @@
+const videos = [
+    {id:0, categoriesID: 0, titre: "Titre1", description:"bla bla", image: "URL", tendances:0, date:"01/10/2022"},
+    {id:1, categoriesID: 1, titre: "Titre2", description:"bla bla", image: "URL", tendances:1, date:"02/10/2022"},
+    {id:2, categoriesID: 2, titre: "Titre3", description:"bla bla", image: "URL", tendances:0, date:"03/10/2022"},
+    {id:3, categoriesID: 3, titre: "Titre4", description:"bla bla", image: "URL", tendances:1, date:"04/10/2022"},
+    {id:4, categoriesID: 4, titre: "Titre5", description:"bla bla", image: "URL", tendances:0, date:"05/10/2022"},
+    {id:5, categoriesID: 0, titre: "Titre6", description:"bla bla", image: "URL", tendances:1, date:"06/10/2022"},
+    {id:6, categoriesID: 1, titre: "Titre7", description:"bla bla", image: "URL", tendances:0, date:"07/10/2022"},
+    {id:7, categoriesID: 2, titre: "Titre8", description:"bla bla", image: "URL", tendances:1, date:"08/10/2022"},
+    {id:8, categoriesID: 3, titre: "Titre9", description:"bla bla", image: "URL", tendances:0, date:"09/10/2022"},
+    {id:9, categoriesID: 4, titre: "Titre10", description:"bla bla", image: "URL", tendances:1, date:"10/10/2022"},
+    {id:10, categoriesID: 0, titre: "Titre11", description:"bla bla", image: "URL", tendances:0, date:"11/10/2022"}
+] ;
