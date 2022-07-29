6dc7a90b26dd38ac0b2e8f8a7f3a9d61bb425d66
y2Dev <dub-y@tuta.io>
2022-07-29 13:05:40 -0400

    ajout de nom dans catégorie

--- constantes/categories.js
+++ constantes/categories.js
@@ -2,7 +2,7 @@ export const categories = [
     {id:0, titre: "Cinéma" },
     {id:1, titre: "Jeux-vidéo"},
     {id:2, titre: "Musiques" },
-    {id:3, titre: "Titre4"},
-    {id:4, titre: "Titre5"},
+    {id:3, titre: "Actualités"},
+    {id:4, titre: "Vidéoclips"},
 
 ] ;
