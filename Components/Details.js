7f26cc17f51f5f85c29226f28785837b8b231fe1
y2Dev <dub-y@tuta.io>
2022-08-18 16:28:36 -0400

    ajout d'un identifiant aux styles des élémnets du composant Details

--- Components/Details.js
+++ Components/Details.js
@@ -7,12 +7,14 @@ const Details = ({item}) => {
   return (
     <View>
         <Image
+          style={styles.videoselect}
           style={{width:350,height:200}}
           resizeMode="cover"
           // source={item.image}
         />
       <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text>
       <Button
+              style={styles.boutonlecture}
               title={"Lire"}
               onPress={""}
             />
@@ -26,6 +28,12 @@ const Details = ({item}) => {
 export default Details
 
 const styles = StyleSheet.create({
+  // videoselect: {
+  //   margin: 50
+  // },
 
-  
+  boutonlecture: {
+    margin: 100
+  }
+
 })
