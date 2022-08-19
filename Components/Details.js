7fde7c81147297c6f86bfe04dfe102b0ce0486f7
y2Dev <dub-y@tuta.io>
2022-08-19 12:21:40 -0400

    ajout de Lorem Ipsum dans le tableau de données vidéos

--- Components/Details.js
+++ Components/Details.js
@@ -42,7 +42,7 @@ const Details = ({ route, navigation }) => {
           onPress={() => console.log('hello')} />
         </View>
 
-      <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text>
+      <Text></Text>
 
 
     </View>
--- constantes/videos.js
+++ constantes/videos.js
@@ -1,13 +1,100 @@
 export const videos = [
-    {id:0, categoriesID: 0, titre: "Youtubeur", description:"bla bla", image: require('../images/aqviews-mfWsMDdN-Ro-unsplash.jpg'), tendances:0, date:"01/10/2022"},
-    {id:1, categoriesID: 1, titre: "Vidéaste", description:"bla bla", image: require('../images/eyestetix-studio-QNOukv0Jx54-unsplash.jpg'), tendances:1, date:"02/10/2022"},
-    {id:2, categoriesID: 2, titre: "Youtube infini", description:"bla bla", image: require('../images/javier-miranda-xW7d0pvzdDk-unsplash.jpg'), tendances:0, date:"03/10/2022"},
-    {id:3, categoriesID: 3, titre: "Vidéaste", description:"bla bla", image: require('../images/nicole-geri-mmmyiQj8Yvg-unsplash.jpg'), tendances:1, date:"04/10/2022"},
-    {id:4, categoriesID: 4, titre: "Monteur", description:"bla bla", image: require('../images/nubelson-fernandes-ZdOsQiwp0Ss-unsplash.jpg'), tendances:0, date:"05/10/2022"},
-    {id:5, categoriesID: 0, titre: "Youtube TV", description:"bla bla", image: require('../images/shubham-dhage--weorgq0juM-unsplash.jpg'), tendances:1, date:"06/10/2022"},
-    {id:6, categoriesID: 1, titre: "Youtubeur", description:"bla bla", image: require('../images/aqviews-mfWsMDdN-Ro-unsplash.jpg'), tendances:0, date:"07/10/2022"},
-    {id:7, categoriesID: 2, titre: "Youtubeur", description:"bla bla", image: require('../images/aqviews-mfWsMDdN-Ro-unsplash.jpg'), tendances:1, date:"08/10/2022"},
-    {id:8, categoriesID: 3, titre: "Youtubeur", description:"bla bla", image: require('../images/aqviews-mfWsMDdN-Ro-unsplash.jpg'), tendances:0, date:"09/10/2022"},
-    {id:9, categoriesID: 4, titre: "Youtubeur", description:"bla bla", image: require('../images/aqviews-mfWsMDdN-Ro-unsplash.jpg'), tendances:1, date:"10/10/2022"},
-    {id:10, categoriesID: 0, titre: "Youtubeur", description:"bla bla", image: require('../images/aqviews-mfWsMDdN-Ro-unsplash.jpg'), tendances:0, date:"11/10/2022"}
+    {   id:0,
+        categoriesID: 0,
+        titre: "Youtubeur",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image: require('../images/aqviews-mfWsMDdN-Ro-unsplash.jpg'),
+        tendances:0,
+        date:"01/10/2022"
+    },
+
+    {   id:1,
+        categoriesID: 1,
+        titre: "Vidéaste",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image: require('../images/eyestetix-studio-QNOukv0Jx54-unsplash.jpg'),
+        tendances:1,
+        date:"02/10/2022"
+    },
+
+    {   id:2,
+        categoriesID: 2,
+        titre: "Youtube infini",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image: require('../images/javier-miranda-xW7d0pvzdDk-unsplash.jpg'),
+        tendances:0,
+        date:"03/10/2022"
+    },
+
+    {   id:3,
+        categoriesID: 3,
+        titre: "Vidéaste",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image: require('../images/nicole-geri-mmmyiQj8Yvg-unsplash.jpg'),
+        tendances:1,
+        date:"04/10/2022"
+    },
+
+    {   id:4,
+        categoriesID: 4,
+        titre: "Monteur",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image: require('../images/nubelson-fernandes-ZdOsQiwp0Ss-unsplash.jpg'),
+        tendances:0,
+        date:"05/10/2022"
+    },
+
+    {   id:5,
+        categoriesID: 0,
+        titre: "Youtube TV",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image: require('../images/shubham-dhage--weorgq0juM-unsplash.jpg'),
+        tendances:1,
+        date:"06/10/2022"
+    },
+
+    {   id:6,
+        categoriesID: 1,
+        titre: "Youtubeur",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image: require('../images/aqviews-mfWsMDdN-Ro-unsplash.jpg'),
+        tendances:0,
+        date:"07/10/2022"
+    },
+
+    {   id:7,
+        categoriesID: 2,
+        titre: "Youtubeur",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image: require('../images/aqviews-mfWsMDdN-Ro-unsplash.jpg'),
+        tendances:1,
+        date:"08/10/2022"
+    },
+
+    {   id:8,
+        categoriesID: 3,
+        titre: "Youtubeur",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image: require('../images/aqviews-mfWsMDdN-Ro-unsplash.jpg'),
+        tendances:0,
+        date:"09/10/2022"
+    },
+
+    {   id:9,
+        categoriesID: 4,
+        titre: "Youtubeur",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image: require('../images/aqviews-mfWsMDdN-Ro-unsplash.jpg'),
+        tendances:1,
+        date:"10/10/2022"
+    },
+
+    {   id:10,
+        categoriesID: 0,
+        titre: "Youtubeur",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image: require('../images/aqviews-mfWsMDdN-Ro-unsplash.jpg'),
+        tendances:0,
+        date:"11/10/2022"
+    }
 ] ;
